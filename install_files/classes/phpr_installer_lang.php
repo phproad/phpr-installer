3dfedd98c9c54648d0a094445ca7e8b811f722fc
Sam Georges <sam@daftspunk.com>
2013-05-18 17:21:03 +1000

    Mixin defaults to global language variable

--- install_files/classes/phpr_installer_lang.php
+++ install_files/classes/phpr_installer_lang.php
@@ -1,15 +1,21 @@
 <?php
 
-$PHPR_INSTALLER_LANG = isset($PHPR_INSTALLER_LANG) ? $PHPR_INSTALLER_LANG : array();
-
 //
 // Website Information
 // 
 
-$PHPR_INSTALLER_LANG['LABEL_LICENSE_NAME'] = 'Website Name';
-$PHPR_INSTALLER_LANG['ERROR_LICENSE_NAME'] = 'Please enter your website name.';
+$LANG['LABEL_LICENSE_NAME'] = 'Website Name';
+$LANG['ERROR_LICENSE_NAME'] = 'Please enter your website name.';
 
-$PHPR_INSTALLER_LANG['LABEL_INSTALLATION_KEY'] = 'Installation Key';
-$PHPR_INSTALLER_LANG['ERROR_INSTALLATION_KEY'] = 'Please enter installation key.';
+$LANG['LABEL_INSTALLATION_KEY'] = 'Installation Key';
+$LANG['ERROR_INSTALLATION_KEY'] = 'Please enter installation key.';
 
-$PHPR_INSTALLER_LANG['LABEL_KEYLESS_ENTRY'] = 'I do not have an installation key.';
+$LANG['LABEL_KEYLESS_ENTRY'] = 'I do not have an installation key.';
+
+// 
+// Mixin defaults to global language variable
+// 
+
+global $PHPR_INSTALLER_LANG;
+$PHPR_INSTALLER_LANG = isset($PHPR_INSTALLER_LANG) ? $PHPR_INSTALLER_LANG : array();
+$PHPR_INSTALLER_LANG = array_merge($LANG, $PHPR_INSTALLER_LANG);
